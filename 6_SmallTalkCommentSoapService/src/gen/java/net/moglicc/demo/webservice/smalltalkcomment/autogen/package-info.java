ee172b5347d10ca845abc4ce34b76368546d9839
Reik Oberrath <r.oberrath@iks-gmbh.com>
2016-05-25 18:15:18 +0200

    generated sources removed from git repository

--- 6_SmallTalkCommentSoapService/src/gen/java/net/moglicc/demo/webservice/smalltalkcomment/autogen/package-info.java
+++
@@ -1,9 +0,0 @@
-//
-// Diese Datei wurde mit der JavaTM Architecture for XML Binding(JAXB) Reference Implementation, v2.2.11 generiert 
-// Siehe <a href="http://java.sun.com/xml/jaxb">http://java.sun.com/xml/jaxb</a> 
-// Änderungen an dieser Datei gehen bei einer Neukompilierung des Quellschemas verloren. 
-// Generiert: 2016.05.25 um 04:40:30 PM CEST 
-//
-
-@javax.xml.bind.annotation.XmlSchema(namespace = "http://net.moglicc.demo.webservice.smalltalkcomment/", elementFormDefault = javax.xml.bind.annotation.XmlNsForm.QUALIFIED)
-package net.moglicc.demo.webservice.smalltalkcomment.autogen;
